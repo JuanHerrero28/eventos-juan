53a547c17f919800bc6adbfe58d49b53eba05542
JuanHerrero28 <juanmanuelherrero281991@gmail.com>
2022-07-19 23:02:26 -0300

    FireBase

--- src/Components/ItemListContainer/ItemListContainer.js
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -2,20 +2,8 @@ import Title from "../Title/Title";
 import ItemList from "../ItemList/ItemList";
 import React, {useState, useEffect} from "react";
 import { useParams } from 'react-router-dom'
+import { getFirestore, collection, getDocs, query, where } from "firebase/firestore";
 
-const eventos = [
-
-    {id: 1, banda: 'Product 1', category: 'rock', image: '/img/banda 3.jpg', lugar: 'Av. Federico Lacroze 3455, C1426 EAB', fecha: '26 de julio', horario: '21:00 a 23:00hs',  price: 3500,  stock: 10},
-    {id: 2, banda: 'Product 1', category: 'rock', image: '/img/banda 3.jpg', lugar: 'Av. Federico Lacroze 3455, C1426 EAB', fecha: '26 de julio', horario: '21:00 a 23:00hs',  price: 1500,  stock: 10},
-    {id: 3, banda: 'Product 1', category: 'pop', image: '/img/banda 3.jpg', lugar: 'Av. Federico Lacroze 3455, C1426 EAB', fecha: '26 de julio', horario: '21:00 a 23:00hs',  price: 4500,  stock: 10},
-    {id: 4, banda: 'Product 1', category: 'rock', image: '/img/banda 3.jpg', lugar: 'Av. Federico Lacroze 3455, C1426 EAB', fecha: '26 de julio', horario: '21:00 a 23:00hs',  price: 3500,  stock: 10},
-    {id: 5, banda: 'Product 1', category: 'pop', image: '/img/banda 3.jpg', lugar: 'Av. Federico Lacroze 3455, C1426 EAB', fecha: '26 de julio', horario: '21:00 a 23:00hs',  price: 3500,  stock: 10},
-    {id: 6, banda: 'Product 1', category: 'rock', image: '/img/banda 3.jpg', lugar: 'Av. Federico Lacroze 3455, C1426 EAB', fecha: '26 de julio', horario: '21:00 a 23:00hs',  price: 3500,  stock: 10},
-    {id: 7, banda: 'Product 1', category: 'indie', image: '/img/banda 3.jpg', lugar: 'Av. Federico Lacroze 3455, C1426 EAB', fecha: '26 de julio', horario: '21:00 a 23:00hs',  price: 1500,  stock: 10},
-    {id: 8, banda: 'Product 1', category: 'indie', image: '/img/banda 3.jpg', lugar: 'Av. Federico Lacroze 3455, C1426 EAB', fecha: '26 de julio', horario: '21:00 a 23:00hs',  price: 3500,  stock: 10},
-    {id: 9, banda: 'Product 1', category: 'rock', image: '/img/banda 3.jpg', lugar: 'Av. Federico Lacroze 3455, C1426 EAB', fecha: '26 de julio', horario: '21:00 a 23:00hs',  price: 3600,  stock: 10},
-    {id: 10, banda: 'Product 1', category: 'indie', image: '/img/banda 3.jpg', lugar: 'Av. Federico Lacroze 3455, C1426 EAB', fecha: '26 de julio', horario: '21:00 a 23:00hs',  price: 3800,  stock: 10},
-]
 
 export const ItemListContainer = ({ texto }) => {
     const [data, setData] = useState([]);
@@ -23,17 +11,17 @@ export const ItemListContainer = ({ texto }) => {
     const {categoryId} = useParams();
 
     useEffect(() => {
-     const getData = new Promise(resolve => {
-        setTimeout(() => {
-        resolve(eventos);  
-        }, 1000)
-     });
 
-     if (categoryId) {
-        getData.then(res => setData (res.filter(evento => evento.category === categoryId)));
-     } else {
-        getData.then(res => setData(res));
-     }
+        const querydb = getFirestore();
+        const queryCollection = collection(querydb, 'productos');
+        if (categoryId) {
+        const queryFilter = query(queryCollection, where('category', '==', categoryId))
+        getDocs(queryFilter)
+          .then(res => setData(res.docs.map(product => ({id: product.id, ...product.data()}))))
+        } else {
+            getDocs(queryCollection)
+            .then(res => setData(res.docs.map(product => ({id: product.id, ...product.data()}))))
+         } 
     
     }, [categoryId])
     
---
+++ src/FireBase/Config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDVdytXqLGTBjh3m81pVmWJZf5hQTLUOas",
+  authDomain: "eventos-juan.firebaseapp.com",
+  projectId: "eventos-juan",
+  storageBucket: "eventos-juan.appspot.com",
+  messagingSenderId: "203887746053",
+  appId: "1:203887746053:web:bcc11e384d70310ff1906c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
